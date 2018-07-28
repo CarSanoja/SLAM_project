2312602ce75c29fdf0d1d04dfd61a8b6ca742609
Carlos Sanoja <wuopcarlos@gmail.com>
2018-07-28 16:02:05 -0700

    init catkin_make

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/robond/Documents/SLAM_project/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/robond/Documents/Localization_Project/devel;/home/robond/Documents/catkin_ws/devel;/home/robond/Documents/cygnus_ws/devel;/opt/ros/kinetic".split(';') if "/home/robond/Documents/Localization_Project/devel;/home/robond/Documents/catkin_ws/devel;/home/robond/Documents/cygnus_ws/devel;/opt/ros/kinetic" != "" else []
